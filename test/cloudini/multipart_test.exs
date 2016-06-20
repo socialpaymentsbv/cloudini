5ec016cc06450ca27660a2c59a695284aa5f2398
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2016-06-20 15:23:18 +0200

    Add test for multipart module

--- test/cloudini/multipart_test.exs
+++ test/cloudini/multipart_test.exs
@@ -13,4 +13,16 @@ defmodule Cloudini.MultipartTest do
     assert String.contains?(body, "Content-Disposition: form-data; name=\"field2\"")
     assert String.contains?(body, "value2")
   end
+
+  test "Encodes fields with files" do
+    {:ok, data} = :file.read_file("fixture/upload/test.gif")
+    files = [{:file, "upload.gif", data}]
+    {boundary, body} = Multipart.render([field1: "value1"], files)
+
+    assert String.starts_with?(body, "--" <> boundary)
+    assert String.contains?(body, "Content-Disposition: form-data; name=\"field1\"")
+    assert String.contains?(body, "value1")
+    assert String.contains?(body, "Content-Disposition: form-data; name=\"file\"; filename=\"upload.gif\"")
+    assert String.contains?(body, "Content-Type: application/octet-stream")
+  end
 end
