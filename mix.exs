5df4e3535b0449bea559e22ba9bf2732f2423252
Wojtek Mach <wojtek@wojtekmach.pl>
2017-04-14 07:43:58 +0200

    Fix Elixir v1.4 warnings

--- mix.exs
+++ mix.exs
@@ -8,31 +8,19 @@ defmodule Cloudini.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: "Cloudinary client",
-     package: package,
-     deps: deps,
+     package: package(),
+     deps: deps(),
      docs: [extras: ["README.md"]]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :crypto, :quintana]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:poison, "~> 2.0"},
      {:quintana, "~> 0.2.1"},
-     {:exvcr, "~> 0.7.4", only: [:dev, :test]}]
+     {:exvcr, "~> 0.8", only: [:dev, :test]}]
   end
 
   defp package do
