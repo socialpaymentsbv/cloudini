cdca74e7f7fd83aa27e80d5396cb75bd233124d6
Wojtek Mach <wojtek@wojtekmach.pl>
2017-07-07 12:09:42 +0200

    Install ex_doc

--- mix.exs
+++ mix.exs
@@ -2,15 +2,20 @@ defmodule Cloudini.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :cloudini,
-     version: "1.2.0",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "Cloudinary client",
-     package: package(),
-     deps: deps(),
-     docs: [extras: ["README.md"]]]
+    [
+      app: :cloudini,
+       version: "1.2.0",
+       elixir: "~> 1.2",
+       build_embedded: Mix.env == :prod,
+       start_permanent: Mix.env == :prod,
+       description: "Cloudinary client",
+       package: package(),
+       deps: deps(),
+       docs: [
+         extras: ["README.md"],
+         main: "readme"
+       ]
+     ]
   end
 
   def application do
@@ -18,13 +23,18 @@ defmodule Cloudini.Mixfile do
   end
 
   defp deps do
-    [{:poison, "~> 2.0"},
-     {:exvcr, "~> 0.8", only: [:test]}]
+    [
+      {:poison, "~> 2.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:exvcr, "~> 0.8", only: [:test]}
+    ]
   end
 
   defp package do
-    [maintainers: ["Adrian Gruntkowski", "Adam Rutkowski"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/socialpaymentsbv/cloudini"}]
+    [
+      maintainers: ["Adrian Gruntkowski", "Adam Rutkowski"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/socialpaymentsbv/cloudini"}
+    ]
   end
 end
